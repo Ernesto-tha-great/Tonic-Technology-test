64cc40f10593859c7a75d74311fe5e552eb8c171
Ernest Nnamdi <ernest@Ernests-MacBook-Air.local>
2021-12-22 05:49:57 +0100

    bottom tabs navigation

---
+++ screens/SendScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SendScreen = () => {
+    return (
+        <View>
+            <Text>swend</Text>
+        </View>
+    )
+}
+
+export default SendScreen
